857758cc0aa5cffa6837b6ff2dd46727ebf39239
Danila Lebedev <l1endqwsa@gmail.com>
2023-08-16 22:03:44 +0300

    testing react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+function Table() {
+    const arr = [1, 2, 3, 4, 5];
+    let html = [];
+    for(let el of arr){
+        html.push(<li>{el}</li>)
+    }
+    return (
+        <ul>
+            {html}
+        </ul>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+      <div>
+          <Table></Table>
+      </div>
+  )
 }
 
 export default App;
